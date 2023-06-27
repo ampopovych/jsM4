417c6654feb11c7eb017260451a49ef2be255bc3
Andriy Popovych <viktpopo@gmail.com>
2023-06-27 15:02:46 +0300

    27061502

--- js/M4/conspect.js
+++ js/M4/conspect.js
@@ -24,5 +24,34 @@ function registerGuest(name, callback) {
 registerGuest("Манго", greet);
 
 
+let val = 7
+function createAdder() {
+   function addNumbers(a, b) {
+     let ret = a + b
+     return ret
+   }
+   return addNumbers
+ }
+ let adder = createAdder()
+ let sum = adder(val, 8)
+console.log('example of function returning a function: ', sum);
+
+
+
+function createCounter() {
+   let counter = 0
+   const myFunction = function() {
+     counter = counter + 1
+     return counter
+   }
+   return myFunction
+ }
+ const increment = createCounter()
+ const c1 = increment()
+ const c2 = increment()
+ const c3 = increment()
+console.log('example increment', c1, c2, c3);
+
+
 
 
