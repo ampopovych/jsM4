f73f3239d20a953b6cb013ee50de248f8183bcfe
Andriy Popovych <viktpopo@gmail.com>
2023-07-04 10:40:13 +0300

    04071040

--- js/M4/autoperevirka.js
+++ js/M4/autoperevirka.js
@@ -1,15 +1,40 @@
 //  2 Задача
-function deliverPizza(pizzaName) {
-  return `Delivering ${pizzaName} pizza.`;
-}
+// function deliverPizza(pizzaName) {
+//   return `Delivering ${pizzaName} pizza.`;
+// }
 
+// function makePizza(pizzaName) {
+//   return `Pizza ${pizzaName} is being prepared, please wait...`;
+// }
+
+// // Chande code below this line
+// function makeMessage(pizzaName, callback) {
+//   return callback ;
+// }
+// makeMessage('Royal Grand', makePizza);
+// makeMessage('Ultracheese', deliverPizza);
+
+
+
+// 4 Задача
+const pizzaPalace = {
+  pizzas: ["Ultracheese", "Smoked", "Four meats"],
+  order(pizzaName, nazva, zamovlennya) { },
+};
+// Change code above this line
+
+// Callback for onSuccess
 function makePizza(pizzaName) {
-  return `Pizza ${pizzaName} is being prepared, please wait...`;
+  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
 }
 
-// Chande code below this line
-function makeMessage(pizzaName, callback) {
-  return callback ;
+// Callback for onError
+function onOrderError(error) {
+  return `Error! ${error}`;
 }
-makeMessage('Royal Grand', makePizza);
-makeMessage('Ultracheese', deliverPizza);
+
+// Method calls with callbacks
+pizzaPalace.order("Smoked", makePizza, onOrderError);
+pizzaPalace.order("Four meats", makePizza, onOrderError);
+pizzaPalace.order("Big Mike", makePizza, onOrderError);
+pizzaPalace.order("Vienna", makePizza, onOrderError);
